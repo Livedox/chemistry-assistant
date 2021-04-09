28c9e269291e3a00d9c377421f5ccfbbaeab5578
Liverdox <inttechkd@gmail.com>
2021-04-10 01:04:21 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
-const dev = process.env.NODE_ENV !== "production";
+let path = "http://localhost:3000";
+if (process.env.VERCEL_URL) server = path.env.VERCEL_URL;
+if (process.env.NODE_ENV === "production") path = "https://chemistry-assistant.ru/";
 
-export const server = dev ? "http://localhost:3000" : "https://chemistry-assistant.ru/";
+
+export const server = path;
