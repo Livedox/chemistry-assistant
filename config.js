9fa8221f70de363779f0871cb56649a7e0d3f771
Liverdox <64814413+Liverdox@users.noreply.github.com>
2021-11-04 04:37:04 +0300

    update server

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 let path = "http://localhost:3000";
-if (process.env.NODE_ENV === "production") path = "https://chemistry-assistant.ru/";
+if (process.env.NODE_ENV === "production") path = "https://chemistry-assistant.ru";
 if (process.env.VERCEL_URL) path = "https://"+process.env.VERCEL_URL;
 
 
---
+++ pages/api/info.ts
@@ -0,0 +1,29 @@
+import Info from "../../models/Info";
+import connectDB from "../../middleware/mongodb";
+import {NextApiRequest, NextApiResponse} from "next";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+    const id: string = JSON.parse(req.body)["id"];
+    try {
+        Info.findOne({path: id}, (error: Error, information: any) => {
+            if(error) {
+                res.status(500).send({message: "Server error"});
+                res.end();
+            }
+            if(information) {
+                res.status(200).send({title: information.title, path: information.path, data: information.data});
+                res.end();
+            } else {
+                res.status(404).send({ message: "Page not found" });
+                res.end();
+            }
+        });
+        return;
+    } catch (error) {
+        res.status(500).send({message: "Server error"});
+        res.end();
+        return;
+    }
+};
+  
+export default connectDB(handler);
