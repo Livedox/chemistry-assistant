3748234c1349c35f9ace00cbc42c867ce735c546
Liverdox <64814413+Liverdox@users.noreply.github.com>
2022-03-23 23:52:38 +0300

    clean trash

--- components/TableSolubility/FixedCell.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function HeaderCell({ el, sup, st }) {
-    return(
-        <th className="fixed-cell" style={st}>{ el }<sup>{ sup }</sup></th>
-    )
-}
